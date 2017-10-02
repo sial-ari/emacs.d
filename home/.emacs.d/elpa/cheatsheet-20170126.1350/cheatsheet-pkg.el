af62edb6393ba4ddf85da9c24f6372785d6eb29f
Vladimir (sial) Todorov <sial@macrew.eu>
2017-10-03 01:14:53 +0300

    Change to homesick style of directories

---
+++ home/.emacs.d/elpa/cheatsheet-20170126.1350/cheatsheet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cheatsheet" "20170126.1350" "create your own cheatsheet" '((emacs "24") (cl-lib "0.5")) :commit "e4f8e0110167ea16a17a74517d1f10cb7ff805b8" :url "http://github.com/darksmile/cheatsheet/" :keywords '("convenience" "usability"))
---
+++ home/.emacs.d/elpa/salt-mode-20170702.246/salt-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "salt-mode" "20170702.246" "Major mode for Salt States" '((emacs "24.4") (yaml-mode "0.0.12") (mmm-mode "0.5.4") (mmm-jinja2 "0.1")) :commit "ea4980b9c9e8e7570086a2558fe7f66c62a528ad" :url "https://github.com/glynnforrest/salt-mode" :keywords '("languages"))
