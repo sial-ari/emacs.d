baed750936d0624562f3bd9f2b7dc4ce4d643330
Vladimir (sial) Todorov <sial@macrew.eu>
2018-10-25 22:29:46 +0300

    Remove helm and smex

--- home/.emacs.d/customizations/navigation.el
+++ home/.emacs.d/customizations/navigation.el
@@ -49,11 +49,14 @@
 ;; Enhances M-x to allow easier execution of commands. Provides
 ;; a filterable list of possible commands in the minibuffer
 ;; http://www.emacswiki.org/emacs/Smex
-(setq smex-save-file (concat user-emacs-directory ".smex-items"))
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
+;;(setq smex-save-file (concat user-emacs-directory ".smex-items"))
+
+;;(smex-initialize)
+;;(global-set-key (kbd "M-x") 'smex)
+
 
 ;; helm
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
+;; (global-set-key (kbd "M-x") #'helm-M-x)
+;; (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+;; (global-set-key (kbd "C-x C-f") #'helm-find-files)
+
