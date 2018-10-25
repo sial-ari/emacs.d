72ad0f28051ff54cb535c8ab7a99037d37bdd699
Vladimir (sial) Todorov <sial@macrew.eu>
2018-10-26 00:10:38 +0300

    Remove elpa from repo

--- home/.emacs.d/elpa/epl-20150517.433/epl-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("epl.el") (22909 45958 259675 261000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epl-autoloads.el ends here
--- home/.emacs.d/elpa/mmm-jinja2-20170313.720/mmm-jinja2-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; mmm-jinja2-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("mmm-jinja2.el") (22919 23153 202227 47000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mmm-jinja2-autoloads.el ends here
