3f109676bfb3922d75320da57c9d32ad3cb73141
Vladimir (sial) Todorov <sial@macrew.eu>
2018-12-04 18:35:52 +0200

    Add help select buffer and org indentation settings

--- home/.emacs.d/lisp/setup-settings.el
+++ home/.emacs.d/lisp/setup-settings.el
@@ -1,5 +1,7 @@
 ;; Free personal key bindings space.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (global-unset-key (kbd "M-j"))
+(setq org-src-tab-acts-natively t)
+(setq help-window-select t)
 
 (provide 'setup-settings)
