145c07d4aaebd18d73f4d5b90fc8c523836afd17
Vladimir (sial) Todorov <sial@macrew.eu>
2019-05-21 19:50:48 +0300

    Add modified custom.el

--- custom.el
+++ custom.el
@@ -3,47 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(compilation-message-face (quote default))
- '(custom-safe-themes
-   (quote
-    ("11e57648ab04915568e558b77541d0e94e69d09c9c54c06075938b6abc0189d8" "8577da1641ed4bdf255341ca92e3d0e49c9f4d574458f09ce78159690442cade" "a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" default)))
- '(help-window-select t)
- '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
- '(highlight-tail-colors
-   (("#49483E" . 0)
-    ("#67930F" . 20)
-    ("#349B8D" . 30)
-    ("#21889B" . 50)
-    ("#968B26" . 60)
-    ("#A45E0A" . 70)
-    ("#A41F99" . 85)
-    ("#49483E" . 100)))
- '(magit-diff-use-overlays nil)
- '(nil nil t)
- '(org-babel-shell-initialize nil)
- '(org-babel-shell-names (quote ("bash")))
  '(package-selected-packages
    (quote
-    (git-auto-commit-mode symon monokai-theme molokai-theme exwm-x ac-geiser geiser helm nlinum pyenv-mode web-mode vagrant-tramp vagrant powerline dired+ yaml-mode websocket smex rainbow-delimiters persp-mode paredit org-bullets multiple-cursors multi-term markdown-mode magit lua-mode load-theme-buffer-local emamux elpy dracula-theme curl-for-url ag ace-window)))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#F92672")
-     (40 . "#CF4F1F")
-     (60 . "#C26C0F")
-     (80 . "#E6DB74")
-     (100 . "#AB8C00")
-     (120 . "#A18F00")
-     (140 . "#989200")
-     (160 . "#8E9500")
-     (180 . "#A6E22E")
-     (200 . "#729A1E")
-     (220 . "#609C3C")
-     (240 . "#4E9D5B")
-     (260 . "#3C9F79")
-     (280 . "#A1EFE4")
-     (300 . "#299BA6")
-     (320 . "#2896B5")
-     (340 . "#2790C3")
-     (360 . "#66D9EF"))))
- '(vc-annotate-very-old-color nil))
+    (pinentry yaml-mode which-key w3m vagrant-tramp vagrant uuidgen use-package tagedit symon super-save smex slime simple-mpc sane-term rainbow-delimiters racket-mode pyenv-mode powerline paredit org-bullets nlinum multiple-cursors multi-term monokai-theme molokai-theme mode-icons mingus markdown-preview-mode magit-popup magit-annex lua-mode json-mode indent-guide helm google-translate god-mode github-clone git-auto-commit-mode ghub+ fzf free-keys flycheck exwm-edit exwm eterm-256color emms emamux elpy dumb-jump dracula-theme diminish desktop-environment cheatsheet anzu all-the-icons ag ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
