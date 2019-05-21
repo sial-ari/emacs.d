c2e5a621079ffc8b1ea64871abaea077cdf210b5
Vladimir (sial) Todorov <sial@macrew.eu>
2019-05-21 20:36:59 +0300

    Add entries to custom

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (pinentry yaml-mode which-key w3m vagrant-tramp vagrant uuidgen use-package tagedit symon super-save smex slime simple-mpc sane-term rainbow-delimiters racket-mode pyenv-mode powerline paredit org-bullets nlinum multiple-cursors multi-term monokai-theme molokai-theme mode-icons mingus markdown-preview-mode magit-popup magit-annex lua-mode json-mode indent-guide helm google-translate god-mode github-clone git-auto-commit-mode ghub+ fzf free-keys flycheck exwm-edit exwm eterm-256color emms emamux elpy dumb-jump dracula-theme diminish desktop-environment cheatsheet anzu all-the-icons ag ace-window))))
+    (escreen use-package-el-get yaml-mode which-key w3m vagrant-tramp vagrant uuidgen use-package tagedit symon super-save smex slime simple-mpc sane-term rainbow-delimiters racket-mode pyenv-mode powerline pinentry paredit org-bullets nlinum multiple-cursors multi-term monokai-theme molokai-theme mode-icons mingus markdown-preview-mode magit-popup magit-annex lua-mode json-mode indent-guide helm google-translate god-mode github-clone git-auto-commit-mode ghub+ fzf free-keys flycheck exwm-edit exwm eterm-256color emms emamux elpy dumb-jump dracula-theme diminish desktop-environment cheatsheet anzu all-the-icons ag ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
