548c5ea749e6f022f5ab3e28dbc2597ad9634070
Vladimir (sial) Todorov <sial@macrew.eu>
2020-03-16 22:32:26 +0200

    Cleanup after emms

--- customizations/misc.el
+++ customizations/misc.el
@@ -34,24 +34,3 @@
 (defun notify-via-messages (title msg)
   "Send notification with TITLE, MSG to message."
   (message "APPOINTMENT: %s" msg))
-
-(defun emms-notifications-dbus (track-name)
-  "Share track name via `D-Bus'."
-  (let ((icon "/usr/share/icons/gnome/24x24/categories/applications-multimedia.png"))
-    (notify-via-notifications "EMMS is now playing:" track-name icon)))
-
-(defun emms-notifications-message (track-name)
-  "Share track name via Emacs minibuffer."
-  (message "EMMS is now playing: %s" track-name))
-
-(setq emms-player-next-function 'emms-notify-and-next)
-
-(defun emms-notify-and-next ()
-  "Send a notification of track and start next."
-  (emms-next-noerror)
-  (let ((track-name (emms-track-description (emms-playlist-current-selected-track))))
-    (cond
-     ((eq notify-method 'notify-via-dbus-notifications)
-      (emms-notifications-dbus track-name))
-     (t (emms-notifications-message track-name)))))
-
