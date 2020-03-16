21ba7b19037447a543ff62da50e81a955b425972
Vladimir (sial) Todorov <sial@macrew.eu>
2020-03-16 22:34:45 +0200

    Change lang to generic and remove test lines

--- init.el
+++ init.el
@@ -2,10 +2,6 @@
 ;; start emacsclient maximized
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
 (require 'package)
 
 (add-to-list 'package-archives
